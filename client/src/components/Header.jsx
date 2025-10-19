ca56fccb10b53434d3a075a9ef4e1cd8931e1445
solo12345689 <sarthakhasija01@gmail.com>
2025-10-19 10:17:33 +0530

    a

--- client/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-export default function Header(){
-  return (
-    <header className="fixed top-4 left-4 z-50 flex items-center gap-3">
-      <div className="bg-green-500 text-black px-2 py-1 rounded font-bold">TV</div>
-      <div className="text-xl font-bold">Gengas Garden</div>
-    </header>
-  )
-}
